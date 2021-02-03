bb4ce077f2f58b294bc7ef534a266a7ea303e824
akirahashimoto <hashimoto7965@gmail.com>
2021-02-03 23:02:26 +0900

    change filename and package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="notify_client",
+    name="notifyclient",
     version="0.1.0",
     author="hashimoto7965@gmail.com",
     author_email="hashimoto7965@gmail.com",
